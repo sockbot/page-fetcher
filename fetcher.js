997d7c1917545fa601eab80920bd641845bd9471
sockbot <github@sockbot.com>
2019-08-01 01:11:34 +0000

    implemented fs write using callback method

--- fetcher.js
+++ fetcher.js
@@ -7,20 +7,31 @@ const filename = args[1];
 
 // console.log(args);
 
-const fetcher = function() {
-  request(url + filename, (error, response, body) => {
-    // console.log('error:', error); // Print the error if one occurred
-    // console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-    // console.log('body:', body); // Print the HTML for the Google homepage.
-
-    fs.writeFile(filename, body, (err) => {
-      if (err) throw err;
-      console.log('The file has been saved');
-    })
-
-  })
+const fetcher = function(callback) {
+  request(url + filename, callback)
   // fs()
 }
 
-
-fetcher();
+const saveFile = function(error, response, data) {
+  if (error) {
+    console.log('Error:', error)
+    process.exit();
+  }
+  // console.log('Response:', response.statusCode)
+  fs.open(filename, 'wx', (err, data) => {
+    if (err) {
+      if (err.code === 'EEXIST') {
+        console.error(`${filename} already exists`);
+        // prompt to overwrite
+        // code goes here
+        return;
+      }
+      throw err;
+    }
+    fs.writeFile(filename, data, (err) => {
+      if (err) throw err;
+      console.log('The file has been saved');
+    });
+  })
+}
+fetcher(saveFile);
